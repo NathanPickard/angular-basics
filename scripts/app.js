be03bc53776edc57f978a173858b21d0d2c50bbb
Nathan Pickard <nathan.pickard87@gmail.com>
2017-01-21 18:31:12 -0800

    Adding to app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,12 @@
-angular.module("todoListApp", []);
+angular.module("todoListApp", [])
+  .controller('mainCtrl', function($scope) {
+    $scope.helloWorld = function() {
+      console.log("Hello there! This is the hellowWorld controller fucntion in the mainCtrl");
+    };
+  })
+  
+  .controller('coolCtrl', function($scope){
+    $scope.whoAmI = function() {
+      console.log("hello there, this is the coolCtrl function!");
+    };
+  });
