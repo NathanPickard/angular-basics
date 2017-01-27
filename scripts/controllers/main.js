e7042b70b527007bca594210f9ab634481b5e3c8
Nathan Pickard <nathan.pickard87@gmail.com>
2017-01-26 21:40:46 -0800

    CSS and reorganized code

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -2,31 +2,29 @@
 
 angular.module('todoListApp')
 .controller('mainCtrl', function($scope, dataService) {
-    
-    $scope.addTodo = function() {
-      var todo = {name: "This is a new todo."};
-      $scope.todos.unshift(todo);
-    };
-
-    $scope.hellowWorld = dataService.hellowWorld;
-
-    dataService.getTodos(function(response) {
-      console.log(response.data);
-      return response.data;
+  $scope.addTodo = function() {
+    var todo = {name: "This is a new todo."};
+    $scope.todos.unshift(todo);
+  };
+  
+  $scope.helloWorld = dataService.helloWorld;
+  
+  dataService.getTodos(function(response) { 
+      console.log(response.data);  
       $scope.todos = response.data;
     });
-
-    $scope.deleteTodo = function(todo, $index) {
-      dataService.deleteTodo(todo);
-      $scope.todos.splice($index, 1);
-    };
-
-    $scope.saveTodos = function() {
-      var filteredTodos = $scope.todos.filter(function(todo) {
-        if(todo.edited) {
-          return todo;
-        };
-      })
-      dataService.saveTodo(filteredtodos);
-    };
-  })
+  
+  $scope.deleteTodo = function(todo, $index) {
+    dataService.deleteTodo(todo);
+    $scope.todos.splice($index, 1);
+  };
+  
+  $scope.saveTodos = function() {
+    var filteredTodos = $scope.todos.filter(function(todo) {
+      if(todo.edited) {
+        return todo;
+      };
+    });
+    dataService.saveTodos(filteredTodos);
+  };
+})
