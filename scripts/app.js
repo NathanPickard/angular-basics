8c93bc344c783939d4f60c9c31ed784440a1b883
Nathan Pickard <nathan.pickard87@gmail.com>
2017-01-26 21:29:49 -0800

    Added directives

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,1 @@
-angular.module("todoListApp", [])
-
-  .controller('mainCtrl', function($scope) {
-    $scope.learningNgChange = function() {
-      console.log("An input changed!");
-    };
-
-    $scope.todos = [
-      {"name": "clean the house"},
-      {"name": "water the dog"},
-      {"name": "feed the lawn"},
-      {"name": "pay bills"},
-      {"name": "run"},
-      {"name": "swim"}
-    ]
-
-  });
+angular.module("todoListApp", []);
